8f9bac42b13aac7ea6089c4c69b30b308e0de50e
Ian <i.r.butterworth@gmail.com>
2020-05-09 16:27:38 -0400

    various

--- src/SystemBenchmark.jl
+++ src/SystemBenchmark.jl
@@ -4,9 +4,12 @@ using CuArrays
 using CSV
 using DataFrames
 using InteractiveUtils
+using LinearAlgebra
 using Logging
 using ProgressMeter
 using VideoIO
+
+include("utils.jl")
 
 export sysbenchmark, compare, compareToRef, saveBenchmark, readBenchmark
 
@@ -16,108 +19,115 @@ function __init__()
     HAS_GPU[] = CuArrays.functional()
 end
 
+function getInfoField(info::String, field::String)::String
+    value = split(info, field)[2]
+    return string(strip(split(value,"\n")[1]))
+end
+
 function getSystemInfo()
     buf = PipeBuffer()
     InteractiveUtils.versioninfo(buf, verbose=false)
     systeminfo = read(buf, String)
-    HAS_GPU[] && (systeminfo *= string("  GPU: $(CuArrays.CUDAdrv.name(CuArrays.CUDAdrv.device()))"))
-    return systeminfo
+    
+    df = DataFrame(cat=String[], testname=String[], res=Union{String,Float64}[])
+    push!(df, ["info","SysBenchVer",string(pkgversion())])
+    push!(df, ["info","JuliaVer",getInfoField(systeminfo, "Julia Version")])
+    push!(df, ["info","OS",getInfoField(systeminfo, "OS:")])
+    push!(df, ["info","CPU",getInfoField(systeminfo, "CPU:")])
+    push!(df, ["info","WORD_SIZE",getInfoField(systeminfo, "WORD_SIZE:")])
+    push!(df, ["info","LIBM",getInfoField(systeminfo, "LIBM:")])
+    push!(df, ["info","LLVM",getInfoField(systeminfo, "LLVM:")])
+    if HAS_GPU[] 
+        push!(df, ["info","GPU",CuArrays.CUDAdrv.name(CuArrays.CUDAdrv.device())])
+    else
+        push!(df, ["info","GPU",""])
+    end
+    return df
 end
 
 function saveBenchmark(path::String, res::DataFrame)
-    systeminfo = getSystemInfo()
     open(path, "w") do io
-        println(io, systeminfo)
-        println(io, "--INFO END--")
-        println(io,"cat,testname,ms")
         CSV.write(io, res, append=true)
     end
 end
 function readBenchmark(path::String)
-    lines = readlines(path)
-    divider = findfirst(occursin.("--INFO END--",lines))
-    s = open(f->read(f, String), path)
-    systeminfo = split(s,"--INFO END--")[1]
-    res = DataFrame(CSV.File(path, skipto=divider+2, header=divider+1))
-    return systeminfo, res
+    return DataFrame(CSV.File(path))
 end
 
 function compare(ref::DataFrame, test::DataFrame)
-	df = DataFrame(cat=String[], testname=String[], ref_ms=Float64[], test_ms=Float64[], factor=Float64[])
+	df = DataFrame(cat=String[], testname=String[], ref_res=Union{String,Float64}[], test_res=Union{String,Float64}[], factor=Union{String,Float64}[])
     for testname in unique(test.testname)
 		testrow = test[test.testname .== testname, :]
-		refrow = ref[ref.testname .== testname, :]
-    	push!(df, Dict(:cat=>testrow.cat[1], 
-			:testname=>testname, 
-			:ref_ms=>refrow.ms[1], 
-			:test_ms=>testrow.ms[1], 
-			:factor=>testrow.ms[1] ./ refrow.ms[1]
-			))
+        refrow = ref[ref.testname .== testname, :]
+        if testrow.cat[1] == "info"
+            push!(df, Dict(:cat=>testrow.cat[1], 
+                :testname=>testname, 
+                :ref_res=>refrow.res[1], 
+                :test_res=>testrow.res[1], 
+                :factor=>""
+                ))
+        else
+            push!(df, Dict(:cat=>testrow.cat[1], 
+                :testname=>testname, 
+                :ref_res=>refrow.res[1], 
+                :test_res=>testrow.res[1], 
+                :factor=>testrow.res[1] ./ refrow.res[1]
+                ))
+        end
 	end
 	return df
-end
-
-function compare(ref_sysinfo::String, ref::DataFrame, test_sysinfo::String, test::DataFrame)
-    println("Reference system ----------------------")
-    println(ref_sysinfo)
-    println("Test system ---------------------------")
-    println(test_sysinfo)
-    println("")
-    return compare(ref::DataFrame, test::DataFrame)
 end
 
 compareToRef() = compareToRef(sysbenchmark(printsysinfo = false))
 
 function compareToRef(test::DataFrame; refname="1-linux-i7-2.6GHz-GTX1650.txt")
-    test_sysinfo = getSystemInfo()
-    ref_sysinfo, ref = readBenchmark(joinpath(dirname(@__DIR__), "ref", refname))
-    return compare(string(ref_sysinfo), ref, test_sysinfo, test)
+    ref = readBenchmark(joinpath(dirname(@__DIR__), "ref", refname))
+    return compare(ref, test)
 end
 
 function sysbenchmark(;printsysinfo = true)
-    ntests = 12
-    systeminfo = getSystemInfo()
-
+    ntests = 17
     if HAS_GPU[]
         ntests += 1
     else
         @info "CuArrays.functional() == false. No usable GPU detected"
     end
 
-    df = DataFrame(cat=String[], testname=String[], ms=Float64[])
+    df = getSystemInfo() #initialize DataFrame with system info 
     prog = ProgressMeter.Progress(ntests) 
     prog.desc = "CPU tests"
-    t = @benchmark x * x setup=(x=rand()); append!(df, DataFrame(cat="cpu", testname="FloatMul", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark sin(x) setup=(x=rand()); append!(df, DataFrame(cat="cpu", testname="FloatSin", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark x .* x setup=(x=rand(10)); append!(df, DataFrame(cat="cpu", testname="VecMulBroad", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark x * x setup=(x=rand(Float32, 100, 100)); append!(df, DataFrame(cat="cpu", testname="CPUMatMul", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark x .* x setup=(x=rand(Float32, 100, 100)); append!(df, DataFrame(cat="cpu", testname="MatMulBroad", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark x .* x setup=(x=rand(10,10,10)); append!(df, DataFrame(cat="cpu", testname="3DMulBroad", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark writevideo(imgstack) setup=(imgstack=map(x->rand(UInt8,100,100), 1:100)); append!(df, DataFrame(cat="cpu", testname="FFMPEGH264Write", ms=median(t).time / 1e6)); next!(prog)
+    t = @benchmark x * x setup=(x=rand()); append!(df, DataFrame(cat="cpu", testname="FloatMul", res=median(t).time / 1e6)); next!(prog)
     t = @benchmark a * b + c setup=(a=rand(),b=rand(),c=rand()); append!(df, DataFrame(cat="cpu", testname="FusedMulAdd", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark sin(x) setup=(x=rand()); append!(df, DataFrame(cat="cpu", testname="FloatSin", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark x .* x setup=(x=rand(10)); append!(df, DataFrame(cat="cpu", testname="VecMulBroad", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark x * x setup=(x=rand(Float32, 100, 100)); append!(df, DataFrame(cat="cpu", testname="CPUMatMul", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark x .* x setup=(x=rand(Float32, 100, 100)); append!(df, DataFrame(cat="cpu", testname="MatMulBroad", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark x .* x setup=(x=rand(10,10,10)); append!(df, DataFrame(cat="cpu", testname="3DMulBroad", res=median(t).time / 1e6)); next!(prog)
     append!(df, DataFrame(cat="cpu", testname="peakflops", res=LinearAlgebra.peakflops())); next!(prog)
+   
+    t = @benchmark writevideo(imgstack) setup=(imgstack=map(x->rand(UInt8,100,100), 1:100)); append!(df, DataFrame(cat="cpu", testname="FFMPEGH264Write", res=median(t).time / 1e6)); next!(prog)
     isfile(joinpath(@__DIR__, "testvideo.mp4")) && rm(joinpath(@__DIR__, "testvideo.mp4"))
 
     if HAS_GPU[]
         prog.desc = "GPU tests"
         x=cu(rand(Float32,100,100))
-        t = @benchmark $x * $x; append!(df, DataFrame(cat="gpu", testname="GPUMatMul", ms=median(t).time / 1e6)); next!(prog)
+        t = @benchmark $x * $x; append!(df, DataFrame(cat="gpu", testname="GPUMatMul", res=median(t).time / 1e6)); next!(prog)
     end
 
     prog.desc = "Memory tests"
-    t = @benchmark deepcopy(x) setup=(x=rand(UInt8,1000)); append!(df, DataFrame(cat="mem", testname="DeepCopy", ms=median(t).time / 1e6)); next!(prog)
+    t = @benchmark deepcopy(x) setup=(x=rand(UInt8,1000)); append!(df, DataFrame(cat="mem", testname="DeepCopy", res=median(t).time / 1e6)); next!(prog)
     
     prog.desc = "Disk IO tests"
-    t = @benchmark tempwrite(x) setup=(x=rand(UInt8,1000)); append!(df, DataFrame(cat="diskio", testname="DiskWrite1KB", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark tempwrite(x) setup=(x=rand(UInt8,1000000)); append!(df, DataFrame(cat="diskio", testname="DiskWrite1MB", ms=median(t).time / 1e6)); next!(prog)
-    t = @benchmark tempread(path) setup=(path = tempwrite(rand(UInt8,1000), delete=false)); append!(df, DataFrame(cat="diskio", testname="DiskRead1KB", ms=median(t).time / 1e6)); next!(prog)
+    t = @benchmark tempwrite(x) setup=(x=rand(UInt8,1000)); append!(df, DataFrame(cat="diskio", testname="DiskWrite1KB", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark tempwrite(x) setup=(x=rand(UInt8,1000000)); append!(df, DataFrame(cat="diskio", testname="DiskWrite1MB", res=median(t).time / 1e6)); next!(prog)
+    t = @benchmark tempread(path) setup=(path = tempwrite(rand(UInt8,1000), delete=false)); append!(df, DataFrame(cat="diskio", testname="DiskRead1KB", res=median(t).time / 1e6)); next!(prog)
     isfile(joinpath(@__DIR__, "testwrite.dat")) && rm(joinpath(@__DIR__, "testwrite.dat"))
-    t = @benchmark tempread(path) setup=(path = tempwrite(rand(UInt8,1000000), delete=false)); append!(df, DataFrame(cat="diskio", testname="DiskRead1MB", ms=median(t).time / 1e6)); next!(prog)
+    t = @benchmark tempread(path) setup=(path = tempwrite(rand(UInt8,1000000), delete=false)); append!(df, DataFrame(cat="diskio", testname="DiskRead1MB", res=median(t).time / 1e6)); next!(prog)
     isfile(joinpath(@__DIR__, "testwrite.dat")) && rm(joinpath(@__DIR__, "testwrite.dat"))
     
 
     prog.desc = "Julia loading tests"
-    t = @benchmark runjulia("1+1"); append!(df, DataFrame(cat="loading", testname="JuliaLoad", ms=median(t).time / 1e6)); next!(prog)
+    t = @benchmark runjulia("1+1"); append!(df, DataFrame(cat="loading", testname="JuliaLoad", res=median(t).time / 1e6)); next!(prog)
     juliatime = median(t).time / 1e6
 
     prog.desc = "Compilation tests"
@@ -132,7 +142,6 @@ function sysbenchmark(;printsysinfo = true)
     deleteat!(LOAD_PATH,1); deleteat!(DEPOT_PATH,1)
 
     finish!(prog)
-    printsysinfo && println(systeminfo)
     return df
 end
 ## CPU
---
+++ src/utils.jl
@@ -0,0 +1,6 @@
+using Pkg
+function pkgversion()
+    projecttoml_filename = joinpath(dirname(dirname(@__FILE__)),"Project.toml")
+    projecttoml_parsed = Pkg.TOML.parse(read(projecttoml_filename, String))
+    return VersionNumber(projecttoml_parsed["version"])
+end
