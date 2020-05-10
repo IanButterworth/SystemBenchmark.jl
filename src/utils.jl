a0e9d118b209e440d71e4fccb49c3b155db79305
Ian <i.r.butterworth@gmail.com>
2020-05-10 13:03:43 -0400

    join submitted results into a single dataframe

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,6 @@
 using Pkg
 using GitHub
+using DataFrames
 
 function pkgversion()
     projecttoml_filename = joinpath(dirname(dirname(@__FILE__)),"Project.toml")
@@ -18,9 +19,14 @@ function readprinteddataframe(str::String)
     select!(df, Not([1,2,8]))
 end
 
-function getsubmittedbenchmarks(repo::String="ianshmean/SystemBenchmark.jl",issue::Int=8)
+function getsubmittedbenchmarks(repo::String="ianshmean/SystemBenchmark.jl",issue::Int=8, refname="ref.txt")
     comments = GitHub.comments(repo, issue, :issue)[1]
     results = DataFrame[]
+    i = 1
+    ref = readbenchmark(joinpath(dirname(@__DIR__), "ref", refname))
+    master_res = DataFrame(cat=["info","info"],testname=["user","datetime"],res=["ref","2020-05-10"])
+    append!(master_res, ref)
+    rename!(master_res, [:cat,:testname,:ref])
     for comment in comments
         if occursin(".txt", comment.body) && occursin("https://github.com/$repo/files/", comment.body)
             resulturl =  string("https://github.com/$repo/files/", split(split(comment.body,"https://github.com/$repo/files/")[2],".txt")[1],".txt")
@@ -28,13 +34,15 @@ function getsubmittedbenchmarks(repo::String="ianshmean/SystemBenchmark.jl",issu
             datetime = comment.updated_at
             file = download(resulturl)
             res = readbenchmark(file)
-            if :test_res in names(res)
+            if "test_res" in names(res)
                 res = DataFrame(cat=res.cat, testname=res.testname, res=res.test_res)
             end
             res_formatted = DataFrame(cat=["info","info"],testname=["user","datetime"],res=[username,datetime])
             append!(res_formatted, res)
-            push!(results,res_formatted)
+            rename!(res_formatted, [:cat,:testname,Symbol("res_$i")])
+            master_res = DataFrames.innerjoin(master_res, res_formatted, on = [:cat,:testname])
+            i += 1
         end
     end
-    return results
+    return master_res
 end
