2bf8c536c23162eb7d54dce05707cc3dc860f261
Ian Butterworth <i.r.butterworth@gmail.com>
2021-08-13 20:36:04 -0400

    add check and guard for no PAT on CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,12 @@ using SystemBenchmark
     res2 = readbenchmark(path);
     comp = comparetoref(res);
     show(comp, allrows=true, allcols=true)
-    
-    crowd = getsubmittedbenchmarks()
-    println("") #tidy test printing
+
+    if haskey(ENV, "CI") && get(ENV, "PERSONAL_ACCESS_TOKEN", "") == ""
+        @warn """No PERSONAL_ACCESS_TOKEN ENV var value provided, so the crowdsource analysis tests cannot run in CI.
+            This may be because you're PR-ing a fork branch."""
+    else
+        crowd = getsubmittedbenchmarks()
+        println("") #tidy test printing
+    end
 end
